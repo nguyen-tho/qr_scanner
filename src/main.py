69227cfd248ba933915921338cfc7dc08e7cdc11
Nguyễn Công Thọ <69784527+nguyen-tho@users.noreply.github.com>
2025-07-08 07:36:44 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
+import cv2
 import qr_detection
 import qr_decode
-import cv2
 
 def main():
     cap = cv2.VideoCapture(0)
@@ -14,10 +14,8 @@ def main():
         if not ret:
             print("Error: Could not read frame.")
             break
-
         # Perform QR code detection
         qr_codes = qr_detection.qr_detection(frame)
-        
         if qr_codes is None or len(qr_codes) == 0:
             print("No QR codes detected.")
             # No need for 'continue' here, just let it display the original frame
@@ -25,7 +23,6 @@ def main():
             print(f"Detected {len(qr_codes)} QR codes.")
             # Draw detected QR codes on the frame
             frame, confidence = qr_detection.draw_qr_codes(frame, qr_codes)
-        
         if confidence > 0.8: 
             # Decode the QR code if confidence is high enough
             decoded_result = qr_decode.decode_qr_code(frame)
@@ -40,7 +37,6 @@ def main():
         if cv2.waitKey(1) & 0xFF == ord('q'):
             print("Exiting...")
             break
-
     cap.release()
     cv2.destroyAllWindows()
 
