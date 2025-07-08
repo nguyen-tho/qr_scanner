5513a81b1a4acae7d88ff716ac3ba86d53f513a5
Nguyễn Công Thọ <69784527+nguyen-tho@users.noreply.github.com>
2025-07-08 12:51:46 +0700

    Update qr_decode.py

--- src/qr_decode.py
+++ src/qr_decode.py
@@ -1,6 +1,8 @@
+"""QR code reader"""
 from qreader import QReader
 
 def decode_qr_code(image):
+    """Read and return the content of the QR code"""
     qr_reader = QReader()
     result = qr_reader.detect_and_decode(image)
     return result
